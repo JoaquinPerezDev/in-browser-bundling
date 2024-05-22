3b311531fe626e76eb112ff2891d2cfa2c61a548
LucasPerezDev@gmail.com <LucasPerezDev@gmail.com>
2024-05-22 17:08:59 -0400

    Created basic form element with state

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,26 @@
 import ReactDOM from "react-dom";
+import { useState } from "react";
 
 const App = () => {
-  return <h1>hi</h1>;
+  const [input, setInput] = useState("");
+  const [code, setCode] = useState("");
+
+  const onClick = () => {
+    console.log(input);
+  };
+
+  return (
+    <h1>
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </h1>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
